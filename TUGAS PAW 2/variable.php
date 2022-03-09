08bf6a6f4a6d8fc043e66e20e121a636ce7dd147
Fiqry-Wahyu-Diky <fiqridiky01@gmail.com>
2022-03-09 23:17:38 +0700

    Tugas 2 menambahkan file variabel,php

---
+++ TUGAS PAW 2/variable.php
@@ -0,0 +1,24 @@
+<?php
+    $x = 2;
+    $y = 5;
+    $z = 10;
+
+    echo "penjumlahan";
+    echo "<br>";
+    $penjumlahan = $x + $y + $z;
+    echo $x . " + ". $y. " + ". $z. " =". $penjumlahan;
+    echo "<br>";
+    echo "<br>";
+    
+    echo "pengurangan";
+    echo "<br>";
+    $pengurangan = $z - $x;
+    echo $z . " - " . $x . ' = ' . $pengurangan;
+    echo '<br>';
+    echo '<br>';
+
+    echo "perkalian";
+    echo "<br>";
+    $perkalian = $x * $y * $z;
+    echo $x . " * " . $y . "*" . $z . " = " . $perkalian; 
+?>
