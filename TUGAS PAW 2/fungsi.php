ff0079242192e63074ab19714f1f1f944a50241a
Fiqry-Wahyu-Diky <fiqridiky01@gmail.com>
2022-03-09 23:31:12 +0700

    Tugas 2 menambahkan file fungsi.php

---
+++ TUGAS PAW 2/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+    function sayHello ($nama){
+        echo "selamat datang, hallo " . $nama . '<br>';
+    }
+sayHello ('Fiqry');
+sayHello ('Wahyu');
+sayHello ('Diky');
+sayHello ('Wicaksono');
+?>
