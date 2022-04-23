00365ce8d60ac480589bbfef040c42071799515f
Fiqry-Wahyu-Diky <fiqridiky01@gmail.com>
2022-04-23 14:15:55 +0700

    rubah UTS

--- TUGAS PAW 2/fungsi.php
+++ TUGAS PAW 2/fungsi.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
     function sayHello ($nama){
         echo "selamat datang, hallo " . $nama . '<br>';
     }
@@ -6,4 +6,20 @@ sayHello ('Fiqry');
 sayHello ('Wahyu');
 sayHello ('Diky');
 sayHello ('Wicaksono');
+?> -->
+
+<?php
+    function cekBilGanjilGenap ($bilangan){
+        if($bilangan%2==0){
+            echo $hasil = 'angka' .  $bilangan . " merupakan bilangan genap";
+        }
+        else{
+            echo $hasil =  $bilangan . " merupakan bilangan ganjil";
+        }
+    return $hasil;
+    };
+
+cekBilGanjilGenap (5);
+echo '<br>';
+cekBilGanjilGenap (10);
 ?>
